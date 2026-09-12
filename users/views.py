d915f400f86cf8d90fb3a663192fd4811be5ebdc
dima.moisieienko <dimonmail09@gmail.com>
2026-09-13 01:42:10 +0300

    created super user logout functionality and added base template

--- users/views.py
+++ users/views.py
@@ -1,8 +1,28 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login
 from .forms import LoginForm
 
 
 def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(
+                request,
+                username=data['username'],
+                password=data['password']
+            )
+
+            if user is not None:
+                login(request, user)
+                return HttpResponse('User logged in')
+            else:
+                return HttpResponse('Invalid login details')
+
+    else:
         form = LoginForm()
-        return render(request, 'users/login.html', {'form': form})
+
+    return render(request, 'users/login.html', {'form': form})
